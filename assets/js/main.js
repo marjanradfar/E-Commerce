0d0c4c9083f1f3d5528b868756aa6b159e80cb17
Marjan <info.marjan.radfar@gmail.com>
2024-02-18 21:11:09 +0330

    creat swiper in home page with npm

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
 import '../style/main.scss';
+
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+const swiper = new Swiper(".singleImg", {
+    scssMode:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    modules: [Navigation, Pagination],
+
+});
