98ef23753fee32265f399eeaa1863a9d3f91dee1
Marjan <info.marjan.radfar@gmail.com>
2024-02-20 13:54:28 +0330

    create file swiper.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,5 @@
 import '../style/main.scss';
+import  './swiper.js';
 
-import Swiper from 'swiper';
-import 'swiper/css';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-const swiper = new Swiper(".singleImg", {
-    scssMode:true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-    modules: [Navigation, Pagination],
 
-});
+
